bc1c7fed0374c7ebda02066d0900a54429267956
PrachiNadgir <142209416+PrachiNadgir@users.noreply.github.com>
2024-12-03 17:30:52 +0000

    datatypes

---
+++ JS/datatypes.js
@@ -0,0 +1,14 @@
+// alert(3+3) This is not allowed as we are using nodejs not browser
+
+// datatypes
+// String
+// BigInt
+// number = 2 to the power of 53 or 52
+// Boolean
+// null = standalone value
+// undefined = not known
+// symbol - use for uniqueness
+// object
+
+// console.log(typeof null) -- object
+// console.log(typeof undefined) -- undefined
