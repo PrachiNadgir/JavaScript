0aada9895cced3459f15afe9ff40d597b37db6f4
PrachiNadgir <142209416+PrachiNadgir@users.noreply.github.com>
2024-12-04 06:11:08 +0000

    Comparison

---
+++ JS/Comparison.js
@@ -0,0 +1,19 @@
+//comparing same datatype
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+//comparing different datatype 
+console.log("2">1);
+console.log("2"<1);//Sometimes its not predictable
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);//assignment n comparison works differently n the comparison op treats null as a num that is 0
+//in case of undefined the returning value is false not like the null
+
+//===(strict check it also checks the datatype )
+console.log(2===2);
+
