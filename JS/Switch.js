042efde2c9a30f181426ad43e6b6ebca86597366
PrachiNadgir <142209416+PrachiNadgir@users.noreply.github.com>
2024-12-06 16:06:13 +0000

    switch

---
+++ JS/Switch.js
@@ -0,0 +1,45 @@
+// switch (key) {//case sensitive
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const month = 3
+switch (month) {
+    case 1:
+        console.log("Jan")
+        break;
+    case 2:
+        console.log("Feb")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+
+    default:
+        break;
+}
+
+const day = "Monday"
+switch(day){
+case "Monday":
+        console.log("Mon")
+        break;
+    case "Tuesday":
+        console.log("tues")
+        break;
+    case "Wednesday":
+        console.log("wed")
+        break;
+    case "Thursday":
+        console.log("Thu")
+        break;
+
+    default:
+console.log('Default matches')
+        break;}
