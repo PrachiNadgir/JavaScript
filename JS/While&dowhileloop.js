7ba10cecfbcba5e9262b2f74ee5dff1fa2a864d4
PrachiNadgir <142209416+PrachiNadgir@users.noreply.github.com>
2024-12-07 03:35:24 +0000

    while and dowhile loop

---
+++ JS/While&dowhileloop.js
@@ -0,0 +1,29 @@
+//while loop
+// let i =0;
+// while(condition){
+//     ++/--
+// }
+// let i = 0;
+// while(i<=10){
+//     console.log(`The value is ${i}`);
+//     i=i+2
+// }
+// let array = ["Icecream","Chocolate","Biscuits"]
+// let index = 0
+// while(index < array.length){
+//     console.log(`The value is ${array[index]}`);
+//     index++
+// }
+
+//do while loop
+
+// do{
+//     pehele kaam
+//     fir Condition
+//     while(condition)
+// }
+let score = 100
+do{
+console.log(`The value is ${score}`)
+score++
+}while(score<=200);
